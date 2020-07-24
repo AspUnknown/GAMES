86a3e700864b7d07ab6e00bb34f028b824d69b95
AspUnknown <67780254+AspUnknown@users.noreply.github.com>
2020-07-24 11:38:38 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -61,6 +61,6 @@ figlet "2048" | lolcat && echo Installing..................... | lolcat
 echo -e "\e[032m"
 pkg install git -y && pkg install wget -y && pkg install clang -y && wget https://raw.githubusercontent.com/mevdschee/2048.c/master/2048.c && sleep 2 && gcc -o 2048 2048.c
 
-echo "Visit www.LearnTermux.tech To learn Basic To Advance About Termux" | lolcat -a
+echo "Visit https://t.me/learntermuxhacking and Advance hacking" | lolcat -a
 echo ""
 echo -e '\033[1mType ./game.sh to start the Termux-Games\033[0m' | lolcat -a
